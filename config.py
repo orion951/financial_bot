754db3aa004863095645e707b407ff6b82ffbffa
orion951 <andreyserezhkin@mail.ru>
2022-08-14 17:30:56 +0300

    third commit

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ user = "andrey"
 password = "1234qwer"
 db_name = "finance_bot_db"
 
-TOKEN = '5455554178:AAHD4PfHQ3WNZx0Tg-hssQxZXZfIGd2MwvM'
+TOKEN = '5455554178:AAHD4PfHQ3WNZx0Tg-hssQxZXZfIGd2MwvM' 
